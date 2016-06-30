55abb5e90baf97cc3eef779a87d527b4610b5b80
Andreas Heumaier <aheumaier@users.noreply.github.com>
2016-06-30 11:08:55 +0200

    Update v2.rb

--- lib/puppet/provider/cloudwatch_alarm/v2.rb
+++ lib/puppet/provider/cloudwatch_alarm/v2.rb
@@ -35,12 +35,14 @@ Puppet::Type.type(:cloudwatch_alarm).provide(:v2, :parent => PuppetX::Puppetlabs
   def self.alarm_to_hash(region, alarm)
     actions = alarm.alarm_actions
 
-    #if (! actions.first.nil?) and (actions.first.start_with?('arn:aws'))
-    #  response = autoscaling_client(region).describe_policies(
-    #    policy_names: actions
-    #  )
-    #  actions = response.scaling_policies.collect(&:policy_name)
-    #end
+    unless  actions.first.nil?
+      if actions.first.start_with?('arn:aws')
+        response = autoscaling_client(region).describe_policies(
+          policy_names: actions
+        )
+        actions = response.scaling_policies.collect(&:policy_name)
+      end
+    end
 
     {
       name: alarm.alarm_name,
